58d1b727eceb69b932c622941f808188ffac03a5
Wren Hawthorne <wren.hawthorne@madetech.com>
2026-02-19 21:55:56 +0000

    add repl, main fn

--- src/01/monkey/main.go
+++ src/01/monkey/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"go-interpreter/src/01/monkey/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Ook! Hello %s! This is the Monkey programming language!\n",
+		user.Username)
+
+	fmt.Printf("Feel free to type in some commands\n")
+
+	repl.Start(os.Stdin, os.Stdout)
+}
