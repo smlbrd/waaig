3a3394d462222de02ea78d9229c7c9c4896d48d7
Wren Hawthorne <wren.hawthorne@madetech.com>
2026-02-19 20:57:10 +0000

    add identifier, letter helper

--- src/01/monkey/lexer/lexer.go
+++ src/01/monkey/lexer/lexer.go
@@ -54,6 +54,13 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 	l.readChar()
 	return tok
@@ -61,4 +68,20 @@ func (l *Lexer) NextToken() token.Token {
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+// read identifier, advance position until encountering non-letter character
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' ||
+		'A' <= ch && ch <= 'Z' ||
+		// treat underscore as a letter
+		ch == '_'
 }
